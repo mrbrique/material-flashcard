7284482af5ec6f6c77eab3614f121becbab593e1
mrbrique <locus.chan.dev@gmail.com>
2022-03-02 11:20:13 +0800

    parse codepoints.csv instead of codepoints.json

--- src/app/material/shared/codepoint.model.ts
+++ src/app/material/shared/codepoint.model.ts
@@ -5,4 +5,14 @@ export class Codepoint {
   code: string;
 
   index: number;
+
+
+  static fromJson(json: any): Codepoint {
+    let cp: Codepoint = new Codepoint();
+    cp.name = json.name;
+    cp.code = json.codepoint;
+    cp.index = json.index;
+
+    return cp;
+  }
 }
--- src/app/service/codepoint.service.ts
+++ src/app/service/codepoint.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { find, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+import * as Papa from "papaparse";
 
 import { Material } from '../material/shared/material.model';
 import { Codepoint } from '../material/shared/codepoint.model';
@@ -15,22 +17,53 @@ import { Codepoint } from '../material/shared/codepoint.model';
 
 export class CodepointService {
 
-  private codepointUrl: string = 'assets/codepoints.json';
+  private codepointUrl: string = 'assets/codepoints.csv';
+
+  private codepoints: Codepoint[];
 
   constructor(private http: HttpClient) { }
 
+  // load codepoint csv and store in Codepoints[]
+  private loadCodepoint(): Observable<Codepoint[]> {
+    return new Observable(observer => {
+      Papa.parse(this.codepointUrl, {
+        download: true,
+        header: true,
+        complete: function(results, file) {
+          this.codepoints = new Array();
+          // initialize the array
+          for (var o of results.data) {
+            // constructor each json to Codepoint
+            this.codepoints.push(Codepoint.fromJson(o));
+          }
+          // delivery the value to subscribers
+          observer.next(this.codepoints);
+        },
+        error: function(err, file, inputElem, reason) {
+          console.log(reason);
+        }
+      })
+    });
+  }
+
   getCodepoints(): Observable<Codepoint[]> {
-    return this.http.get<Codepoint[]>(this.codepointUrl);
+    if (this.codepoints) {
+      // if codepoints is loaded, return it
+      return of(this.codepoints);
+    } else {
+      // otherwise, subscribe it
+      return this.loadCodepoint();
+    }
   }
 
   getCodepoint(name: string): Observable<Codepoint> {
-    return this.http.get<Codepoint[]>(this.codepointUrl).pipe(
+    return this.getCodepoints().pipe(
       map(codepoints => codepoints.find(cp => cp.name === name)!)
     );
   }
 
   getIndexedCodepoint(index: number): Observable<Codepoint> {
-    return this.http.get<Codepoint[]>(this.codepointUrl).pipe(
+    return this.getCodepoints().pipe(
       map(codepoints => codepoints.find(cp => cp.index === index)!)
     );
   }
