b1662955ee7382ae7858d19300960120ede09dbf
mrbrique <locus.chan.dev@gmail.com>
2022-05-31 15:42:01 +0800

    mobile splash screen

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -13,7 +13,8 @@ export class SplashScreenComponent implements OnInit {
   cardDisplay: Material[] = new Array();
   // flashcard display material
 
-  readonly cardStack: Number = 5;
+  readonly deckCount: Number = 5;
+  // number of cards in deck
 
   constructor(
     private codepointService: CodepointService
@@ -21,7 +22,7 @@ export class SplashScreenComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    for (let i = 0; i < this.cardStack; i++) {
+    for (let i = 0; i < this.deckCount; i++) {
       // rnadom 5 cards for splash screen
       this.codepointService.getIndexedCodepoint(Math.floor(Math.random() * 999)).subscribe(
         cp => this.cardDisplay.push(new Material(cp))
