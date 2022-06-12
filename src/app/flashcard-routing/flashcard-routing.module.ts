0a6cc3b1f3c4ed7f617e148908befa2de7960dc6
mrbrique <locus.chan.dev@gmail.com>
2022-06-13 03:09:02 +0800

    remove ShuffleComponent

--- src/app/flashcard-routing/flashcard-routing.module.ts
+++ src/app/flashcard-routing/flashcard-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FlashcardComponent } from '../components/flashcard/flashcard.component';
 import { DeckComponent } from '../components/deck/deck.component';
 import { HandComponent } from '../components/hand/hand.component';
-import { ShuffleComponent } from '../components/shuffle/shuffle.component';
+import { FlashcardShuffleComponent } from '../components/flashcard-shuffle/flashcard-shuffle.component';
 import { FlashcardCatalogComponent } from '../components/flashcard-catalog/flashcard-catalog.component';
 import { FlashcardWallComponent } from '../components/flashcard-wall/flashcard-wall.component';
 import { SplashScreenComponent } from '../components/splash-screen/splash-screen.component';
@@ -12,7 +12,7 @@ import { FanDeckComponent } from '../components/fan-deck/fan-deck.component';
 
 const routes: Routes = [
   { path: 'card/:item', component: HandComponent },
-  { path: 'shuffle', component: ShuffleComponent },
+  { path: 'shuffle', component: FlashcardShuffleComponent },
   { path: 'catalog', component: FlashcardCatalogComponent },
   { path: 'wall', component: FlashcardWallComponent },
   { path: 'fan', component: FanDeckComponent },
