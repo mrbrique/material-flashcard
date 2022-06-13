3d7e9fbeb7fdf8c86064ab419c287ab2755a5b88
mrbrique <locus.chan.dev@gmail.com>
2022-06-13 15:08:02 +0800

    FlashcardShuffle template

--- src/app/components/flashcard-shuffle/flashcard-shuffle.component.ts
+++ src/app/components/flashcard-shuffle/flashcard-shuffle.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+
+import { MatIconRegistry } from "@angular/material/icon";
+
+
+import { Material } from '../../material/shared/material.model';
+import { Codepoint } from '../../material/shared/codepoint.model';
+import { CodepointService } from '../../service/codepoint.service';
+
+
 
 @Component({
   selector: 'flashcard-shuffle',
@@ -6,10 +16,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flashcard-shuffle.component.less']
 })
 export class FlashcardShuffleComponent implements OnInit {
+  materials: Material[] = [];
 
-  constructor() { }
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private codepointService: CodepointService
+  ) {
+    this.matIconRegistry.addSvgIcon("swipe-up",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/img/swipe-up.svg")
+    );
+  }
 
   ngOnInit(): void {
+    this.codepointService.getCodepoints().subscribe(
+      (codepoints: Codepoint[]) => {
+        for (let cp of codepoints) {
+          this.materials.push(new Material(cp));
+        }
+      }
+    );
   }
 
 }
